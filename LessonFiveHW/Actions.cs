db2b1e508f4d52a3ad7694a96d528b6f6c3de181
Константин Тарасов <kostya1cn@gmail.com>
2023-09-10 23:21:55 +0300

    Lesson Five Home Work

---
+++ LessonFiveHW/Actions.cs
@@ -0,0 +1,38 @@
+﻿public static class Actions
+{
+    public static Result InfoAction()
+    {
+        Result result = new Result
+        {
+            Status = LogLevel.Info,
+            Message = "This is an info message",
+            DateTime = DateTime.Now
+        };
+        Logger.Instance.Log(result);
+        return result;
+    }
+
+    public static Result WarningAction()
+    {
+        Result result = new Result
+        {
+            Status = LogLevel.Warning,
+            Message = "This is a warning message",
+            DateTime = DateTime.Now
+        };
+        Logger.Instance.Log(result);
+        return result;
+    }
+
+    public static Result ErrorAction()
+    {
+        Result result = new Result
+        {
+            Status = LogLevel.Error,
+            Message = "This is an error message",
+            DateTime = DateTime.Now
+        };
+        Logger.Instance.Log(result);
+        return result;
+    }
+}
---
+++ LessonFiveHW/Logger.cs
@@ -0,0 +1,49 @@
+﻿public class Logger
+{
+    private static Logger? instance;
+    public List<Result> logList;
+    private LogLevel logLevel;
+
+    private Logger()
+    {
+        logList = new List<Result>();
+    }
+
+    public static Logger Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+    }
+
+    public LogLevel LogLevel
+    {
+        get { return logLevel; }
+        set { logLevel = value; }
+    }
+
+    public void Log(Result result)
+    {
+        if (result.Status == logLevel)
+        {
+            logList.Add(result);
+            Console.WriteLine($"{result.DateTime}: {result.Status}: {result.Message}");
+        }
+    }
+
+    public void LogToFile(string filePath)
+    {
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            foreach (Result result in logList)
+            {
+                writer.WriteLine($"{result.DateTime}: {result.Status}: {result.Message}");
+            }
+        }
+    }
+}
